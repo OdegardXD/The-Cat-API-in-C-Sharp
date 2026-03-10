793ec826567c1bb666ddcad292f83b02a251f747
Raymond Ødegård <raymond.odegard.personal@gmail.com>
2026-03-10 09:21:35 +0100

    Added more code comments Added a "Copy Cat" button Added a note saying that the first cat request might take a second

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,14 @@
         {
             CatRequestButton = new Button();
             PictureBox = new PictureBox();
+            UILabel1 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)PictureBox).BeginInit();
             SuspendLayout();
             // 
             // CatRequestButton
             // 
-            CatRequestButton.Location = new Point(191, 366);
+            CatRequestButton.Location = new Point(191, 386);
             CatRequestButton.Name = "CatRequestButton";
             CatRequestButton.Size = new Size(90, 23);
             CatRequestButton.TabIndex = 0;
@@ -47,26 +49,54 @@
             // 
             PictureBox.Location = new Point(12, 12);
             PictureBox.Name = "PictureBox";
-            PictureBox.Size = new Size(452, 268);
+            PictureBox.Size = new Size(452, 348);
             PictureBox.TabIndex = 1;
             PictureBox.TabStop = false;
+            // 
+            // UILabel1
+            // 
+            UILabel1.AutoSize = true;
+            UILabel1.BackColor = Color.Transparent;
+            UILabel1.ForeColor = SystemColors.ActiveCaptionText;
+            UILabel1.Location = new Point(12, 426);
+            UILabel1.Name = "UILabel1";
+            UILabel1.Size = new Size(237, 15);
+            UILabel1.TabIndex = 2;
+            UILabel1.Text = "Note: First image grab might take a second!";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(334, 418);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Copy Cat to Clipboard";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(479, 450);
+            Controls.Add(button1);
+            Controls.Add(UILabel1);
             Controls.Add(PictureBox);
             Controls.Add(CatRequestButton);
             Name = "Form1";
+            ShowIcon = false;
             Text = "The Cat API In C#!";
             ((System.ComponentModel.ISupportInitialize)PictureBox).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button CatRequestButton;
         private PictureBox PictureBox;
+        private Label UILabel1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,10 +10,10 @@ namespace The_Cat_API_in_C_
         public Form1()
         {
             InitializeComponent();
-            PictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            PictureBox.SizeMode = PictureBoxSizeMode.Zoom; // Make it so that the Picture Box fits the entirety of the image/gif. (i couldnt find the option in the properties of the Picture Box lol)
         }
 
-        static readonly HttpClient client = new HttpClient();
+        static readonly HttpClient client = new HttpClient(); // Have this outside the button so that it doesnt run everytime the user requests a new cat for optimization.
 
         private async void CatRequestButton_Click(object sender, EventArgs e)
         {
@@ -23,6 +23,18 @@ namespace The_Cat_API_in_C_
             Stream stream = await client.GetStreamAsync(CatURL); // Step 4: Get the image from the link and store it in a stream.
             Image CatImage = Image.FromStream(stream); // Step 5: Store the image from the stream in a image variable (Not really needed but i like storing it in a variable incase i wanna do something else with ths code in the future).
             PictureBox.Image = CatImage; // Step 6: Display the now downloaded image of a cat in the piture box!
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (PictureBox.Image != null) // If PictureBox isnt empty or null then:
+            {
+                Clipboard.SetImage(PictureBox.Image); // Copy the current cat (image or gif) to clipboard!
+            }
+            else // And if the message box is empty or null then:
+            {
+                MessageBox.Show("There is no cat to cop :(\n Please request a cat first!", "Error! No cat :("); // Show a message box saying that the user needs to request a cat before trying to copy the cat.
+            }
         }
     }
 }
